c28afb70522babb43a3ab34022a9c022a2c58c8b
bbwrldhlspc <yonathanofficial@gmail.com>
2018-02-21 13:18:34 +0200

    Homework done

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -10,52 +10,51 @@ public class Main {
 
         Scanner scanner = new Scanner(System.in);
         String answer = scanner.next();
+        Random random = new Random();
 
         while (answer.equals("yes")) {
+            int myNum = random.nextInt(100) + 1;
 
 //            for (int a = 0; a <= 1; a++) {
-                Random random = new Random();
 
-                System.out.println("I've thought of number from 1 to 100, try to guess it.");
+            System.out.println("I've thought of number from 1 to 100, try to guess it.");
 
-                int myNum = random.nextInt(100) + 1;
+            System.out.println(myNum);
 
-                System.out.println(myNum);
+            for (int i = 1; i <= 10; i++) {
+                int userNum = scanner.nextInt();
+                if (i <= 9) {
 
-                for (int i = 1; i <= 10; i++) {
-                    int userNum = scanner.nextInt();
-                    if (i <= 9) {
-
-                        if (myNum > userNum) {
-                            System.out.println("Your number is smaller then the one I've thought of.");
-                            System.out.println("Number of tries left: " + (10 - i));
-                        }
-                        if (myNum < userNum) {
-                            System.out.println("Your number is bigger then the one I've thought of.");
-                            System.out.println("Number of tries left: " + (10 - i));
-                        }
-                        if (myNum == userNum) {
-                            System.out.println("Congrats, you won!!!");
-                            System.out.println("---------------------");
-                            break;
-                        }
-
+                    if (myNum > userNum) {
+                        System.out.println("Your number is smaller then the one I've thought of.");
+                        System.out.println("Number of tries left: " + (10 - i));
                     }
-                    if (i == 10) {
-
-                        if (myNum == userNum) {
-                            System.out.println("Congrats, you won!!!");
-                            System.out.println("---------------------");
-                        } else {
-                            System.out.println("You lost...");
-                            break;
-                        }
-
+                    if (myNum < userNum) {
+                        System.out.println("Your number is bigger then the one I've thought of.");
+                        System.out.println("Number of tries left: " + (10 - i));
                     }
+                    if (myNum == userNum) {
+                        System.out.println("Congrats, you won!!!");
+                        System.out.println("---------------------");
+                        break;
+                    }
+
+                }
+                if (i == 10) {
+
+                    if (myNum == userNum) {
+                        System.out.println("Congrats, you won!!!");
+                        System.out.println("---------------------");
+                    } else {
+                        System.out.println("You lost...");
+                        break;
+                    }
+
+                }
 
 //                int userNum = scanner.nextInt();
 
-                    //System.out.println(userNum);
+                //System.out.println(userNum);
 
 //                if (myNum > userNum) {
 //                    System.out.println("Your number is smaller then the one I've thought of.");
@@ -67,7 +66,7 @@ public class Main {
 //                    System.out.println("Congrats, bitch!!!!");
 //                    break;
 //                }
-                }
+            }
 //            }
             System.out.println("Do you want to keep playing? yes/no");
             answer = scanner.next();
