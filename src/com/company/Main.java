b86494eb5606e504709bd2f0cb9c24166db69933
bbwrldhlspc <yonathanofficial@gmail.com>
2018-02-21 03:50:25 +0200

    Homework done

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,39 +6,72 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        for (int a = 0; a <= 9999; a++) {
-            Random random = new Random();
-            Scanner scanner = new Scanner(System.in);
+        System.out.println("Do you want to play a game with me? yes/no");
 
-            System.out.println("I've thought of number from 1 to 100, try to guess it.");
+        Scanner scanner = new Scanner(System.in);
+        String answer = scanner.next();
 
-            int myNum = random.nextInt(100) + 1;
+        while (answer.equals("yes")) {
 
-            //System.out.println(myNum);
+//            for (int a = 0; a <= 1; a++) {
+                Random random = new Random();
 
-            for (int i = 1; i <= 10; i++) {
-                if (i<=9) {
-                    System.out.println("Number of tries left: " + (11 - i));
+                System.out.println("I've thought of number from 1 to 100, try to guess it.");
+
+                int myNum = random.nextInt(100) + 1;
+
+                System.out.println(myNum);
+
+                for (int i = 1; i <= 10; i++) {
+                    int userNum = scanner.nextInt();
+                    if (i <= 9) {
+
+                        if (myNum > userNum) {
+                            System.out.println("Your number is smaller then the one I've thought of.");
+                            System.out.println("Number of tries left: " + (10 - i));
+                        }
+                        if (myNum < userNum) {
+                            System.out.println("Your number is bigger then the one I've thought of.");
+                            System.out.println("Number of tries left: " + (10 - i));
+                        }
+                        if (myNum == userNum) {
+                            System.out.println("Congrats, you won!!!");
+                            System.out.println("---------------------");
+                            break;
+                        }
+
+                    }
+                    if (i == 10) {
+
+                        if (myNum == userNum) {
+                            System.out.println("Congrats, you won!!!");
+                            System.out.println("---------------------");
+                        } else {
+                            System.out.println("You lost...");
+                            break;
+                        }
+
+                    }
+
+//                int userNum = scanner.nextInt();
+
+                    //System.out.println(userNum);
+
+//                if (myNum > userNum) {
+//                    System.out.println("Your number is smaller then the one I've thought of.");
+//                }
+//                if (myNum < userNum) {
+//                    System.out.println("Your number is bigger then the one I've thought of.");
+//                }
+//                if (myNum == userNum) {
+//                    System.out.println("Congrats, bitch!!!!");
+//                    break;
+//                }
                 }
-                if (i==10) {
-                    System.out.println("It's your last try this game.");
-                }
-
-                int userNum = scanner.nextInt();
-
-                //System.out.println(userNum);
-
-                if (myNum > userNum) {
-                    System.out.println("Your number is smaller then the one I've thought of.");
-                }
-                if (myNum < userNum) {
-                    System.out.println("Your number is bigger then the one I've thought of.");
-                }
-                if (myNum == userNum) {
-                    System.out.println("Congrats, bitch!!!!");
-                    break;
-                }
-            }
+//            }
+            System.out.println("Do you want to keep playing? yes/no");
+            answer = scanner.next();
         }
+        System.out.println("Good bye, have a nice day!");
     }
 }
