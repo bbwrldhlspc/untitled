335574e7a48d229f192294d8f95849254b78a301
bbwrldhlspc <yonathanofficial@gmail.com>
2018-03-01 02:50:35 +0200

    Homework done

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -16,80 +16,60 @@ public class Main {
             String answer = askAnswer();
             Random random = new Random();
 
-
             while (answer.equals("yes")) {
-
                 int myNum = random.nextInt(100) + 1;
 
-//            for (int a = 0; a <= 1; a++) {
-
                 System.out.println("I've thought of number from 1 to 100, try to guess it.");
-
+                /*Cheat*/
                 System.out.println(myNum);
 
                 for (int i = 1; i <= 10; i++) {
                     int userNum = askNumber();
                     if (i <= 9) {
-
                         if (myNum > userNum) {
                             System.out.println("Your number is smaller then the one I've thought of.");
                             System.out.println("Number of tries left: " + (10 - i));
                         }
+
                         if (myNum < userNum) {
                             System.out.println("Your number is bigger then the one I've thought of.");
                             System.out.println("Number of tries left: " + (10 - i));
                         }
+
                         if (myNum == userNum) {
                             System.out.println("Congrats, you won!!!");
                             System.out.println("---------------------");
                             break;
                         }
-
                     }
-                    if (i == 10) {
 
+                    if (i == 10) {
                         if (myNum == userNum) {
                             System.out.println("Congrats, you won!!!");
                             System.out.println("---------------------");
-                        } else {
+                        }
+
+                        else {
                             System.out.println("You lost...");
                             break;
                         }
-
                     }
-
-//                int userNum = scanner.nextInt();
-
-                    //System.out.println(userNum);
-
-//                if (myNum > userNum) {
-//                    System.out.println("Your number is smaller then the one I've thought of.");
-//                }
-//                if (myNum < userNum) {
-//                    System.out.println("Your number is bigger then the one I've thought of.");
-//                }
-//                if (myNum == userNum) {
-//                    System.out.println("Congrats, bitch!!!!");
-//                    break;
-//                }
                 }
 
-//            }
                 System.out.println("Do you want to keep playing? yes/no");
                 answer = askAnswer();
             }
             System.out.println("Good bye, have a nice day!");
 
-//
         } catch (NoSuchElementException e) {
-            System.out.println("Goodbye, you've pressed leave combintation");
-
+            System.out.println("Goodbye, you've pressed leave combination");
         }
     }
 
     static String askAnswer() {
         for (; ; ) {
             String answer = scanner.next();
+            answer = answer.toLowerCase();
             if (answer.equals("yes")) {
                 return answer;
             }
@@ -101,7 +81,6 @@ public class Main {
     }
 
     static int askNumber() {
-
         for (; ; ) {
             try {
                 int num = scanner.nextInt();
@@ -111,19 +90,8 @@ public class Main {
             } catch (InputMismatchException e) {
                 scanner.next();
                 System.out.println("oops. You must enter a number...");
-                System.out.println("Wrong number. Must be between 1 and 100");
             }
         }
     }
-// static int askNumber() {
-//        int num;
-//        do {
-//            num = scanner.nextInt();
-//            if (num > 100 || num < 1) {
-//                System.out.println("Numer is somewhere between 1 and 100");
-//            }
-//        } while (num > 100 || num < 1);
-//        return num;
-//    }
 }
 
